d682c231be756ffac49ba2c9ca42b994806dce2a
jtb1137 <jtb1137@gmail.com>
2017-07-14 12:11:32 -0400

    add to #menu: while loop, exit/list/option prompt, stock options, list option

--- lib/trending-stocks-gem/cli.rb
+++ lib/trending-stocks-gem/cli.rb
@@ -12,11 +12,36 @@ class TrendingStocksGem::CLI
 	end
 
 	def menu
-		puts "Enter the number or name of the stock you would like to know more about:"
-		input = gets.strip
-		case input
-		when input = 1
-			puts "More info on stock 1"
+		input = nil
+		while input != 'exit'
+			puts "Type 'exit' to quit at any time."
+			puts "Type 'list' to view the list of stocks."
+			puts "Enter the number of the stock you would like to know more about:"
+			input = gets.strip.downcase
+			case input
+			when input = "1"
+				puts "More info on stock 1"
+			when input = "2"
+				puts "More info on stock 2"
+			when input = "3"
+				puts "More info on stock 3"
+			when input = "4"
+				puts "More info on stock 4"
+			when input = "5"
+				puts "More info on stock 5"
+			when input = "6"
+				puts "More info on stock 6"
+			when input = "7"
+				puts "More info on stock 7"
+			when input = "8"
+				puts "More info on stock 8"
+			when input = "9"
+				puts "More info on stock 9"
+			when input = "10"
+				puts "More info on stock 10"
+			when input = "list"
+				list_stocks
+			end
 		end
 	end
 
