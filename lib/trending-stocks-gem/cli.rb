d6a8c73fb2d7cb395f25c79a41add231111f1c8f
jtb1137 <jtb1137@gmail.com>
2017-07-14 11:57:47 -0400

    scrub out #call, #list_stocks, #menu

--- lib/trending-stocks-gem/cli.rb
+++ lib/trending-stocks-gem/cli.rb
@@ -2,6 +2,22 @@ class TrendingStocksGem::CLI
 	
 	def call
 		puts "Today's Trending Stocks"
+		list_stocks
+		menu
+	end
+
+	def list_stocks
+		puts "1. JPMorgan - Last: 91.75 - High: 92.06 - Low 90.58"
+		puts "2. AMD - "
+	end
+
+	def menu
+		puts "Enter the number or name of the stock you would like to know more about:"
+		input = gets.strip
+		case input
+		when input = 1
+			puts "More info on stock 1"
+		end
 	end
 
 end
