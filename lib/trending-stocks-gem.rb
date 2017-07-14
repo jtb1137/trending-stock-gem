2236b6986b72cbbbaa5d9e3cec0f3fe60559cf36
jtb1137 <jtb1137@gmail.com>
2017-07-14 14:28:16 -0400

    add nokogiri

--- lib/trending-stocks-gem.rb
+++ lib/trending-stocks-gem.rb
@@ -2,6 +2,8 @@ require_relative "trending-stocks-gem/version"
 require_relative "trending-stocks-gem/stock.rb"
 require_relative "trending-stocks-gem/cli.rb"
 
+require "nokogiri"
+
 module TrendingStocksGem
   # Your code goes here...
 end
