0180ccf560c58b034bd4cd945463743e1d5e8d46
jtb1137 <jtb1137@gmail.com>
2017-07-17 13:48:51 -0400

    CLI Refresh - #list_stocks currently broken, build out #print_stocks

--- lib/trending-stocks-gem/cli.rb
+++ lib/trending-stocks-gem/cli.rb
@@ -1,17 +1,17 @@
 class TrendingStocksGem::CLI
 	
 	def call
+		#TrendingStocksGem::Scraper ??????
 		puts "Today's Trending Stocks"
 		list_stocks
 		menu
+		exit
 	end
 
 	def list_stocks
-		@stocks = TrendingStocksGem::Stock.current
-		@stocks.each.with_index(1) do |stock, i|
-			puts "#{i}. #{stock.name} - Last: #{stock.last} - High: #{stock.high} - Low: #{stock.low}"
-		end
-	end		
+		puts "1. AMD - Last: 58.71 - Low: 56.03 - High: 59.42"
+		puts "2. Apple - Last: - Low - High"
+	end
 
 	def menu
 		input = nil
@@ -22,18 +22,29 @@ class TrendingStocksGem::CLI
 			
 			input = gets.strip.downcase
 			if input.to_i > 0
-				this_stock = @stocks[input.to_i - 1]
-				puts "#{this_stock.name} - Last: #{this_stock.last} - High: #{this_stock.high} - Low: #{this_stock.low}"
+				print_stock
 			elsif input == "list"
 				list_stocks
 			elsif input == "exit"
-				sign_off
+				exit
 			end
 		end
 	end
 
-	def sign_off
-		puts "Session completed."
+	def print_stock(stock)
+		puts ""
+		puts "----------- #{stock.name} -----------"
+		puts ""
+		puts "Last:           #{stock.location}"
+		puts "Low:            #{stock.head_chef}"
+		puts "High:           #{stock.contact}"
+		puts "Change:         #{stock.change}"
+		puts "Change %:       #{stock.change_percent}" 
+		puts "Trade Volume:   #{stock.volume}"
+	end
+
+	def exit
+		puts "Session complete."
 	end
 
 end
--- lib/trending-stocks-gem/stock.rb
+++ lib/trending-stocks-gem/stock.rb
@@ -7,32 +7,32 @@ class TrendingStocksGem::Stock
 	end
 
 	def self.scrape_stocks
-		stocks = []
 
 		doc = Nokogiri::HTML(open("https://www.investing.com/equities/trending-stocks"))
-		
+
+		doc.css("#trendingInnerContent > table > tbody > tr").each do |tr|
+			
+		end
+
 		stock = self.new
 		stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td.left.bold.plusIconTd.elp > a").text
 		stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(3)").text
 		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(4)").text
 		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(5)").text
+
+
+
+		#stocks = []
+
+		#doc = Nokogiri::HTML(open("https://www.investing.com/equities/trending-stocks"))
 		
-		#binding.pry
-
-		#stock_1 = self.new
-		#stock_1.name = "JPMorgan"
-		#stock_1.last = "91.75"
-		#stock_1.high = "92.06"
-		#stock_1.low = "90.58"
-
-		#stock_2 = self.new
-		#stock_2.name = "AMD"
-		#stock_2.last = "85.75"
-		#stock_2.high = "86.06"
-		#stock_2.low = "84.58"
-
-		#[stock_1, stock_2]
-		stocks << stock
+		#stock = self.new
+		#stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td.left.bold.plusIconTd.elp > a").text
+		#stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(3)").text
+		#stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(4)").text
+		#stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(5)").text
+		
+		
 	end
 
 end
