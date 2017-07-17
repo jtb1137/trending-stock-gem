8547142992885e2f23fda32f11c772a6de407871
jtb1137 <jtb1137@gmail.com>
2017-07-17 14:38:07 -0400

    #print_stock fix

--- lib/trending-stocks-gem/stock.rb
+++ lib/trending-stocks-gem/stock.rb
@@ -8,17 +8,17 @@ class TrendingStocksGem::Stock
 
 	def self.scrape_stocks
 
+		#stocks = []
+
 		#doc = Nokogiri::HTML(open("https://www.investing.com/equities/trending-stocks"))
 
-		#doc.css("#trendingInnerContent > table > tbody > tr").each do |tr|
-			
+		#doc.css("#trendingInnerContent > table > tbody > tr").map do |tr|
+		#	stock_name = doc.search("td.left.bold.plusIconTd.elp > a").text
+		#	stock_last = doc.search("td:nth-child(3)").text
+		#	stock_high = doc.search("td:nth-child(4)").text
+		#	stock_low = doc.search("td:nth-child(5)").text	
+		#	stocks << {name: stock_name, last: stock_last, high: stock_high, low: stock}
 		#end
-
-		#stock = self.new
-		#stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td.left.bold.plusIconTd.elp > a").text
-		#stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(3)").text
-		#stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(4)").text
-		#stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(5)").text
 
 
 
@@ -32,8 +32,69 @@ class TrendingStocksGem::Stock
 		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(4)").text
 		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(1) > td:nth-child(5)").text
 		stocks << stock
+
+		stock = self.new
+		stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(2) > td.left.bold.plusIconTd.elp > a").text
+		stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(2) > td:nth-child(3)").text
+		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(2) > td:nth-child(4)").text
+		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(2) > td:nth-child(5)").text
+		stocks << stock
 		
+		stock = self.new
+		stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(3) > td.left.bold.plusIconTd.elp > a").text
+		stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(3) > td:nth-child(3)").text
+		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(3) > td:nth-child(4)").text
+		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(3) > td:nth-child(5)").text
+		stocks << stock
 		
+		stock = self.new
+		stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(4) > td.left.bold.plusIconTd.elp > a").text
+		stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(4) > td:nth-child(3)").text
+		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(4) > td:nth-child(4)").text
+		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(4) > td:nth-child(5)").text
+		stocks << stock
+
+		stock = self.new
+		stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(5) > td.left.bold.plusIconTd.elp > a").text
+		stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(5) > td:nth-child(3)").text
+		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(5) > td:nth-child(4)").text
+		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(5) > td:nth-child(5)").text
+		stocks << stock
+
+		stock = self.new
+		stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(6) > td.left.bold.plusIconTd.elp > a").text
+		stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(6) > td:nth-child(3)").text
+		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(6) > td:nth-child(4)").text
+		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(6) > td:nth-child(5)").text
+		stocks << stock
+
+		stock = self.new
+		stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(7) > td.left.bold.plusIconTd.elp > a").text
+		stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(7) > td:nth-child(3)").text
+		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(7) > td:nth-child(4)").text
+		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(7) > td:nth-child(5)").text
+		stocks << stock
+
+		stock = self.new
+		stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(8) > td.left.bold.plusIconTd.elp > a").text
+		stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(8) > td:nth-child(3)").text
+		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(8) > td:nth-child(4)").text
+		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(8) > td:nth-child(5)").text
+		stocks << stock
+
+		stock = self.new
+		stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(9) > td.left.bold.plusIconTd.elp > a").text
+		stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(9) > td:nth-child(3)").text
+		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(9) > td:nth-child(4)").text
+		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(9) > td:nth-child(5)").text
+		stocks << stock
+
+		stock = self.new
+		stock.name = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(10) > td.left.bold.plusIconTd.elp > a").text
+		stock.last = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(10) > td:nth-child(3)").text
+		stock.high = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(10) > td:nth-child(4)").text
+		stock.low = doc.search("#trendingInnerContent > table > tbody > tr:nth-child(10) > td:nth-child(5)").text
+		stocks << stock
 	end
 
 end
